cc4a9f41d388856ce8ebaab83d4854c20e591916
aalsuha4 <aalsuha4@kent.edu>
2018-11-08 14:59:11 -0500

    update

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 export FLASK_APP=microblog.py
+export FLASK_DEBUG=1
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME="webcs444@gmail.com"
+export MAIL_PASSWORD="adeeb1234"
 flask run
